db5348d72000f7484fe5558b9fb295d02b3c5362
Laura Alvarez M <lalvar16@eafit.edu.co>
2022-06-21 23:15:19 -0500

    Primera parte del proyecto

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path=require("path");
+const app = express();
+const publicPath=path.resolve(__dirname,"./public");
+app.use(express.static(publicPath));
+/* deja público solo esto */
+
+
+app.listen(3000,()=>{console.log("Servidor corriendo")});
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/home.html"));
+})
+
