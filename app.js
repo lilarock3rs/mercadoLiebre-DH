cf232fec032ffd19e9e01a2b72cb3300936a699b
Laura Alvarez M <lalvar16@eafit.edu.co>
2022-07-05 23:39:46 -0500

    Agregar Formularios

--- app.js
+++ app.js
@@ -13,3 +13,10 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/home.html"));
 })
 
+app.get('/registro',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/registro.html"));
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/login.html"));
+})
+
